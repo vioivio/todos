d5506bcee1198ed617a5c713d205f2c4f3ee2757
vioivio <dadawaq93@gmail.com>
2022-06-01 20:27:47 +0300

    added js

---
+++ main.js
@@ -0,0 +1,34 @@
+const addTaskBtn = document.getElementById('add-task-btn');
+const deskTaskInput = document.getElementById('description-task');
+const todosWrapper = document.querySelector('.todos-wrapper');
+
+let tasks;
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
+
+function Task(description) {
+    this.description = description;
+    this.completed = false 
+}
+
+const createTemplate = (task, index) => {
+    return `
+        
+    `
+}
+
+const fillHtmlList = () => {
+    todosWrapper.innerHTML = "";
+    if(tasks.length > 0) {
+        tasks.forEach((item, index) => {
+            todosWrapper.innerHTML += createTemplate(item, index)
+        })
+    }
+}
+
+const updateLocal =() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+addTaskBtn/addEventListener('click', () => {
+    tasks.push(new Task(deskTaskInput.value))
+    updateLocal();
+})
